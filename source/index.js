90fbc6477ac66829b6197264a2ef8b415f0866a2
Benjamin Lupton <b@lupton.cc>
2020-04-17 20:21:36 +1000

    v1.1.0 - extract-timestamp to 1.2.0

--- source/index.js
+++ source/index.js
@@ -1,39 +1,39 @@
 import {
-  extractYoutubeID,
-  replaceTimestamps,
-  makeYoutubeTimestamp,
+	extractYoutubeID,
+	replaceTimestamps,
+	makeYoutubeTimestamp,
 } from "../node_modules/extract-timestamp/edition-browsers/index.js";
 
 let youtubeID = "";
 
 $.fn.getYoutubeID = function () {
-  return extractYoutubeID(this.get(0));
+	return extractYoutubeID(this.get(0));
 };
 
 $.fn.youtubeTimestamps = function () {
-  const $this = $(this);
+	const $this = $(this);
 
-  if ($this.hasClass("youtube-timestamped")) return $this;
-  $this.addClass("youtube-timestamped");
+	if ($this.hasClass("youtube-timestamped")) return $this;
+	$this.addClass("youtube-timestamped");
 
-  youtubeID =
-    $this.getYoutubeID() || $(document.body).getYoutubeID() || youtubeID;
+	youtubeID =
+		$this.getYoutubeID() || $(document.body).getYoutubeID() || youtubeID;
 
-  if (youtubeID && $this.has('h1:contains("my notes")')) {
-    const html = $this.html();
-    const result = replaceTimestamps(
-      html,
-      function (timestamp) {
-        return makeYoutubeTimestamp(timestamp, youtubeID, " —");
-      },
-      " [-—]"
-    );
-    if (result && html !== result) $this.html(result);
-  }
+	if (youtubeID && $this.has('h1:contains("my notes")')) {
+		const html = $this.html();
+		const result = replaceTimestamps(
+			html,
+			function (timestamp) {
+				return makeYoutubeTimestamp(timestamp, youtubeID, " —");
+			},
+			" [-—]"
+		);
+		if (result && html !== result) $this.html(result);
+	}
 
-  return $this;
+	return $this;
 };
 
 api.decorateCooked(($elem) => $elem.youtubeTimestamps(), {
-  id: "youtubetimestamps-decorator",
+	id: "youtubetimestamps-decorator",
 });
